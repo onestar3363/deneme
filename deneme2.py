03116ce9d64c7f5cd718db5f2fea81bc41e3e975
onestar3363 <101664176+onestar3363@users.noreply.github.com>
2022-07-14 21:33:04 +0300

    Update deneme2.py

--- deneme2.py
+++ deneme2.py
@@ -1,5 +1,8 @@
+import streamlit as st
 import pandas as pd
 import ccxt
+import sqlalchemy
+
 
 exchange=ccxt.currencycom()
 markets= exchange.load_markets()
@@ -50,3 +53,41 @@ commodities=commodities1[commodities1['name'].str.contains('Futures')==False]
 dfc6=pd.concat([americalist, europelist,currencylist,japanlist,cryptolist,indices,commodities], axis=0)
 dfc6['fullname'] = dfc6.iloc[:,0]+'_'+dfc6.iloc[:,1]+'_'+dfc6.iloc[:,2]+'_'+dfc6.iloc[:,3]
 dfc6.iloc[:, [0,6]].to_csv('csymbols.csv',header=False, index=False)
+
+st.set_page_config(layout="wide")
+st.title('Screener')
+start = time.perf_counter()
+@st.cache(suppress_st_warning=True)
+def getdata():
+    if os.path.exists("günlük.db"):
+        os.remove("günlük.db")
+    elif os.path.exists("haftalik.db"):
+        os.remove("haftalik.db")
+    exchange=ccxt.currencycom()
+    markets= exchange.load_markets()    
+    symbols1=pd.read_csv('csymbols.csv',header=None)
+    symbols=symbols1.iloc[:,0].to_list()
+    index = 0
+    fullnames=symbols1.iloc[:,1].to_list()
+    engine=sqlalchemy.create_engine('sqlite:///günlük.db')
+    enginew=sqlalchemy.create_engine('sqlite:///haftalik.db')
+    with st.empty():
+        for ticker,fullname in zip(symbols,fullnames):
+            index += 1
+            try:
+                data2 = exchange.fetch_ohlcv(ticker, timeframe='1d',limit=250) #since=exchange.parse8601('2022-02-13T00:00:00Z'))
+                data3= exchange.fetch_ohlcv(ticker, timeframe='1w',limit=250)
+                st.write(f"⏳ {index,ticker} downloaded")
+            except Exception as e:
+                print(e)
+            else:
+                header = ['Date', 'Open', 'High', 'Low', 'Close', 'Volume']
+                dfc = pd.DataFrame(data2, columns=header)
+                dfc['Date'] = pd.to_datetime(dfc['Date'],unit='ms')
+                dfc['Date'] = dfc['Date'].dt.strftime('%d-%m-%Y')
+                dfc.to_sql(fullname,engine, if_exists='replace')
+                dfc2 = pd.DataFrame(data3, columns=header)
+                dfc2['Date'] = pd.to_datetime(dfc2['Date'],unit='ms')
+                dfc2['Date'] = dfc2['Date'].dt.strftime('%d-%m-%Y')
+                dfc2.to_sql(fullname,enginew, if_exists='replace')
+
